d32b251d27cf2bf37dd6f05af7a524c4dc8944bc
Tarun Sabharwal <tarunsbh@gmail.com>
2022-11-06 23:57:35 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ExpenseTracker} from './component/expense-tracker';
+
+function App() {
+  return (
+    <div className="App">
+      {ExpenseTracker()}
+    </div>
+  )
+}
+
+export default App;
